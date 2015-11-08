c13947229a46224ed8385f489cbef2a3dabde718
Djamel <ddrif.djamel@gmail.com>
2015-11-08 15:22:39 +0100

    classe user créee:
    manque le corps des méthodes.
    Besoin d'une connexion a la BDD
    et la logique JMS

--- src/projetjmsmodandr/utilisateurs/Users.java
+++ src/projetjmsmodandr/utilisateurs/Users.java
@@ -18,7 +18,10 @@ public class Users {
     private String motDePasse;
     private Date derniereConnexion;
     private String villeReceprion;
+    private Date dateDebut;
+    private Date dateFin;
     
+
     public Users(String nom,String prenom,String login,String motDePasse,String villeReceprion){
         this.nom= nom;
         this.prenom=prenom;
@@ -111,5 +114,98 @@ public class Users {
         this.villeReceprion = villeReceprion;
     }
     
-    // mettre les autres méthodes 
+    /**
+     * connection a l'appli
+     * @param login
+     * @param motDePasse
+     * @return 
+     */
+    public String connection(String login, String motDePasse){
+        /**
+         * reste le corps et connection a la 
+         */
+       return "manque la connection a la base ";
+    }
+    
+    /**
+     * se deconnecter
+     * @param login
+     * @return 
+     */
+    public String decconnection(String login){
+        /**
+         * reste le corps et connection a la 
+         */
+       return "manque la connection a la base ";
+    }
+    
+    /**
+     * voir l'historique des tweets, des personnes suivies,.. 
+     * @return 
+     */
+    public String voirHistorique(){
+        /**
+         * reste le corps et connection a la 
+         */
+       return "manque la connection a la base ";
+    }
+    
+    /**
+     * rechercher une personne par son login
+     * @param login
+     * @return 
+     */
+    public String recherche(String login){
+        /**
+         * reste le corps et connection a la 
+         */
+       return "manque la connection a la base ";
+    }
+    
+    /**
+     * s'inscrire au service 
+     * @param login
+     * @return 
+     */
+    public String sinscrire(String login){
+        /**
+         * reste le corps et connection a la 
+         */
+       return "manque la connection a la base ";
+    }
+    
+    /**
+     * diffuser un tweet
+     * @return 
+     */
+    public String diffuser(){
+        /**
+         * reste le corps et connection a la 
+         */
+       return "manque la connection a la base ";
+    }
+    
+    /**
+     * s'abonner a qlq1
+     * @param login
+     * @return 
+     */
+    public String suivre(String login){
+        /**
+         * reste le corps et connection a la 
+         */
+       return "manque la connection a la base ";
+    }
+    
+    /**
+     * se desabonner d'un abonnement
+     * @param login
+     * @return 
+     */
+    public String desabonner(String login){
+        /**
+         * reste le corps et connection a la 
+         */
+       return "manque la connection a la base ";
+    }
 }
