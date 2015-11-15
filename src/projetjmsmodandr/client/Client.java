8cd02c15c389786e26c020fb7e8bcc162a145250
Djamel <ddrif.djamel@gmail.com>
2015-11-15 23:19:19 +0100

    le client ici joue le role du receveur

---
+++ src/projetjmsmodandr/client/Client.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetjmsmodandr.client;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import javax.jms.JMSException;
+import javax.jms.Destination;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+
+/**
+ *
+ * @author Djamel D
+ */
+public class Client {
+    
+}
