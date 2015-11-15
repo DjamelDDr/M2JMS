7f729021ac9d8010f6b8566288b12e80ecbea419
Djamel <ddrif.djamel@gmail.com>
2015-11-16 00:04:52 +0100

    debut
    le tous n'est pas encore fonctionnel mais ça viendra

--- src/projetjmsmodandr/serveur/Reponse.java
+++ src/projetjmsmodandr/serveur/Reponse.java
@@ -5,17 +5,37 @@
  */
 package projetjmsmodandr.serveur;
 
-import javax.jms.MessageProducer;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
+import javax.jms.JMSException;
+import javax.jms.Destination;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 /**
  *
  * @author Djamel D
  * mm fonctionnement que le sender et donc un thread
  */
 public class Reponse extends Thread{
+    
+        Context context = null;
+        ConnectionFactory factory = null;
+        Connection connection = null;
+        String factoryName = "ConnectionFactory";
+        String destName = null;
+        Destination dest = null;
+        Session session = null;
+    
     private MessageProducer sender;
+    
     public Reponse(MessageProducer sender){
         this.sender = sender;
+        
     }
     
     public void run(){
